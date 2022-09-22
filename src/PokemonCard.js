a693411c4c494416b116394a76d3252beaebcdc2
Tim Kelley <timkelley683@gmail.com>
2022-09-22 16:47:51 -0700

    pokedex cards componenets

---
+++ src/PokemonCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./PokemonCard.css";
+import useCardFlipState from "./hooks/hooks";
+
+/* Renders a single pokemon card. */
+function PokemonCard({ front, back, name, stats }) {
+
+  const [isFacingUp, toggleIsFacingUp] = useCardFlipState();
+
+  /* !!! Custom Hook above negates need for code below */
+
+  // const [isFacingUp, setIsFacingUp] = useState(true);
+  // const flipCard = () => {
+  //   setIsFacingUp(isUp => !isUp);
+  // };
+
+  return (
+    <div onClick={toggleIsFacingUp} className="PokemonCard Card">
+      {isFacingUp ? (
+        <div className="PokemonCard-front">
+          <img src={front} alt={`{name} front`} />
+          <div>
+            <p className="PokemonCard-name">{name}</p>
+            <ul className="PokemonCard-stats">
+              {stats.map(stat => (
+                <li key={stat.name}>
+                  <em>{stat.name}</em>: {stat.value}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <div className="PokemonCard-back">
+          <img src={back} alt={`{name} back`} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PokemonCard;
